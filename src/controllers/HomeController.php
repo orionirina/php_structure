32ea02e9b40b4d69264a1b428227ec3fe07bb522
Naina <naina.r@my-overview.com>
2025-08-08 11:41:12 +0300

    home controller

---
+++ src/controllers/HomeController.php
@@ -0,0 +1,11 @@
+<?php
+    require_once 'includes/HttpRequest.php';
+    require_once 'includes/HttpResponse.php';
+
+    class HomeController {
+        public function index(HttpRequest $request, HttpResponse $response) {
+            $response->setData('pageTitle', 'Accueil')
+                    ->render('templates/index.html.php');
+        }
+    }
+?>
